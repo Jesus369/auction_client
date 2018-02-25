e78e54cc4baa8bb5518de5cd440634b47e5b99af
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-25 09:52:32 -0600

    Delete Sell.js

--- src/routes/sell/Sell.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import { graphql } from "react-apollo";
-import gql from "graphql-tag";
-
-class Sell extends Component {
-  render() {
-    return <div>Sell Page</div>;
-  }
-}
